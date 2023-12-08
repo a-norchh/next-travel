b13a07537981fcaed07bd2c4bae4c9762c64ca06
Norchh <a.norawich@gmail.com>
2023-12-08 18:15:21 +0700

    finished the project without animation

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,13 +5,16 @@ type ButtonProps = {
   title: string;
   icon?: string;
   variant: string;
+  full?: boolean;
 };
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={`${variant} flexCenter gap-3 rounded-full border`}
+      className={`${variant} flexCenter gap-3 rounded-full border ${
+        full && "w-full"
+      }`}
     >
       {icon && <Image src={icon} alt="icon" width={24} height={24} />}
       <label className="bold-16 whitespace-nowrap cursor-pointer">
